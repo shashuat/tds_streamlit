e78f7ca583ce572270b7d109eb2c2b06e3089877
Shashwat Sharma <72076441+shashuat@users.noreply.github.com>
2023-04-19 22:24:53 +0530

    Update num_largest.py

--- num_largest.py
+++ num_largest.py
@@ -6,9 +6,9 @@ def find_largest(a, b, c):
 st.title("Find the Largest Number")
 st.write("Enter three numbers to find the largest among them:")
 
-num1 = st.number_input("First number", min_value=-1e20, step=1.0)
-num2 = st.number_input("Second number", min_value=-1e20, step=1.0)
-num3 = st.number_input("Third number", min_value=-1e20, step=1.0)
+num1 = st.number_input("First number")
+num2 = st.number_input("Second number")
+num3 = st.number_input("Third number")
 
 if st.button("Calculate Largest Number"):
     largest = find_largest(num1, num2, num3)
